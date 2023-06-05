d9c0de5cdde33716f1427430553d2d24a40d1a79
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-06-05 04:29:23 +0000

    feat: add mkcert download to create_tls_certs.sh script

--- create_tls_certs.sh
+++ create_tls_certs.sh
@@ -1,4 +1,11 @@
-mkcert -install 
+
+# https://github.com/FiloSottile/mkcert/tree/v1.4.4
+curl -JLO "https://dl.filippo.io/mkcert/latest?for=linux/amd64"
+chmod +x mkcert-v*-linux-amd64
+sudo mv mkcert-v*-linux-amd64 /usr/local/bin/mkcert
+
+
+mkcert -install
 mkcert localhost 127.0.0.1 ::1
 mv localhost+2.pem cert.pem
 mv localhost+2-key.pem key.pem
